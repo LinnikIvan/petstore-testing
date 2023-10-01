29ae71576dd8d38f0dd9664687929bedff57dd7f
AlinaLinnik90 <alina.machmudova.1990@gmail.com>
2023-10-01 11:50:25 +0300

    add dependencies add first junit test

---
+++ src/test/java/org/openapitools/api/PetApiDelegateImplTest.java
@@ -0,0 +1,36 @@
+package org.openapitools.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.openapitools.model.Pet;
+import org.openapitools.repository.PetRepository;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PetApiDelegateImplTest {
+
+    @InjectMocks
+    private PetApiDelegateImpl petApiDelegate;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Test
+    public void name() {
+        Pet pet = new Pet();
+        when(petRepository.save(any())).thenReturn(pet);
+
+        ResponseEntity<Void> response = petApiDelegate.addPet(pet);
+
+        assertThat(response, is(ResponseEntity.ok().build()));
+    }
+
+}
